7a35ca75ced89219ec18c6be166435247998868c
Bysholo <vicentevaldebenitoa@gmail.com>
2023-03-30 00:40:19 -0300

    Tarea 1 - Avances en estrellas

--- Tarea-1.py
+++ Tarea-1.py
@@ -1,13 +1,20 @@
 import pyglet
-from pyglet import shapes, gl
-from pyglet.window import Window, key
-from pyglet.graphics import Batch, draw
+from pyglet import shapes
+from pyglet.window import Window
+from pyglet.graphics import Batch
 from pyglet.app import run
+from random import random
 import numpy as np
 
 # Creación de ventana y bash
-ventana = Window(900,700,"Vamos fak",resizable=True)
+ventana = Window(900,700,"Vamos fak",resizable=False)
 batch = Batch()
+
+y_grid = [0]*10
+x_grid = [0]*10
+for i in range(0,10):
+    y_grid[i] = ventana.height*i/10
+    x_grid[i] = ventana.width*i/10
 
 class Spaceship:
     def __init__(self,
@@ -18,38 +25,56 @@ class Spaceship:
 
         x0 = center[0]
         y0 = center[1]
-        self.lBody = pyglet.shapes.Triangle(x0,y0+80,x0,y0-20,x0-20,y0,
+        self.lBody = shapes.Triangle(x0,y0+80,x0,y0-20,x0-20,y0,
                                     color=(131, 85, 160), batch=batch)
-        self.rBody = pyglet.shapes.Triangle(x0,y0+80,x0,y0-20,x0+20,y0,
+        self.rBody = shapes.Triangle(x0,y0+80,x0,y0-20,x0+20,y0,
                                     color=(131, 85, 160), batch=batch)
         
-        self.lWing = pyglet.shapes.Triangle(x0-16,y0,x0-40,y0+16,x0-80,y0-20,
+        self.lWing = shapes.Triangle(x0-16,y0,x0-40,y0+16,x0-80,y0-20,
                                     color=(89, 77, 157), batch=batch)
-        self.rWing = pyglet.shapes.Triangle(x0+16,y0,x0+40,y0+16,x0+80,y0-20,
+        self.rWing = shapes.Triangle(x0+16,y0,x0+40,y0+16,x0+80,y0-20,
                                     color=(89, 77, 157), batch=batch)
         
-        self.lUpEngine = pyglet.shapes.Triangle(x0-32,y0,x0-24,y0+40,x0-8,y0-8,
+        self.lUpEngine = shapes.Triangle(x0-32,y0,x0-24,y0+40,x0-8,y0-8,
                                     color=(152, 57, 71), batch=batch)
-        self.rUpEngine = pyglet.shapes.Triangle(x0+32,y0,x0+24,y0+40,x0+8,y0-8,
+        self.rUpEngine = shapes.Triangle(x0+32,y0,x0+24,y0+40,x0+8,y0-8,
                                     color=(152, 57, 71), batch=batch)
                 
-        self.lLowEngine = pyglet.shapes.Triangle(x0-32,y0,x0-24,y0-36,x0-8,y0-8,
+        self.lLowEngine = shapes.Triangle(x0-32,y0,x0-24,y0-36,x0-8,y0-8,
                                     color=(152, 57, 71), batch=batch)
-        self.rLowEngine = pyglet.shapes.Triangle(x0+32,y0,x0+24,y0-36,x0+8,y0-8,
+        self.rLowEngine = shapes.Triangle(x0+32,y0,x0+24,y0-36,x0+8,y0-8,
                                     color=(152, 57, 71), batch=batch)
 
-road = pyglet.shapes.Rectangle(x=10, y=10, width=ventana.width-20,
-                                 height=ventana.height-20, color=(80, 80, 80),
-                                 batch=batch)
-Ship1 = Spaceship((ventana.width/2,ventana.height*2/3))
-Ship2 = Spaceship((ventana.width*2/6,ventana.height/2))
-Ship3 = Spaceship((ventana.width*4/6,ventana.height/2))
-Ship4 = Spaceship((ventana.width/6,ventana.height/3))
-Ship5 = Spaceship((ventana.width*3/6,ventana.height/3))
-Ship6 = Spaceship((ventana.width*5/6,ventana.height/3))
+class Star:
+    def __init__(self,x,y,vy) -> None:
+        self.coord = (x,y)
+        self.spd = vy
+
+    def update(self):
+        self.coord += (self.cood(0),self.cood(1) + self.spd)
+
+class Sky():
+    def __init__(self):
+        self.background = shapes.Rectangle(0, 0, ventana.width, ventana.height, color=(5, 5, 30),batch=batch)
+
+    def gen_stars(self):
+        n = random.randint(4,7)
+        samp = random.sample(range(0,10),n)
+        
+
+
+background = Sky()
+
+Ship1 = Spaceship((ventana.width/2,ventana.height/2 + 120))
+Ship2 = Spaceship((ventana.width/2 - 100,ventana.height/2))
+Ship3 = Spaceship((ventana.width/2 + 100,ventana.height/2))
+Ship4 = Spaceship((ventana.width/2,ventana.height/2 - 120))
+Ship5 = Spaceship((ventana.width/2 + 180,ventana.height/2 - 120))
+Ship6 = Spaceship((ventana.width/2 - 180,ventana.height/2 - 120))
 
 @ventana.event
 def on_draw():
+    
     ventana.clear()
     batch.draw()
 
--- Testing.py
+++ Testing.py
@@ -1,3 +1,4 @@
+import math as m
 import pyglet
 import random
 from pyglet import shapes, gl
@@ -6,14 +7,76 @@ from pyglet.graphics import Batch, draw
 from pyglet.app import run
 import numpy as np
 
-# Creación de ventana y bash
-ventana = Window(700,900,"Les fakin go",resizable=True)
-nae = Batch()
+#value = np.empty((), dtype=object)
+#value[()] = (0, 0)
+#a = np.full((10,10),value,dtype=tuple)
+#print(a)
 
-road = pyglet.shapes.Rectangle(x=10, y=10, width=ventana.width-20,
-                                 height=ventana.height-20, color=(80, 80, 80),
-                                 batch=nae)
+#def gen_rand_positions(n):
+#    sampl = random.sample(range(0,10),n)
+#    lista = [0]*10
+#
+#    for i in range(0,n):
+#        lista[sampl[i]] = 1
+#
+#    return lista
+#
+#m=0
+#while m<10000000:
+#    n = random.randint(4,8)
+#    a = gen_rand_positions(n)
+#    sum = 0
+#    for i in range(0,10):
+#        sum += a[i]
+#    if not (n == sum):
+#        print(n,a,sum)
+#        print("te aweonaste")
+#        break
+#    print(m)
+#    m+=1
+#
+#print("puto amo")
 
-poli = pyglet.shapes.Polygon(((20,20),(20,40),(25,50),(50,20)),batch=nae)
+window = pyglet.window.Window(800, 600)
+batch = pyglet.graphics.Batch()
 
-run()
+road = pyglet.shapes.Rectangle(x=10, y=10, width=window.width-20,
+                                 height=window.height-20, color=(80, 80, 80),
+                                 batch=batch)
+
+dirt = pyglet.shapes.Rectangle(x=120, y=120, width=window.width-240,
+                                 height=window.height-240, color=(53, 40, 30),
+                                 batch=batch)
+
+class Car:
+    def __init__(self):
+        self.body = pyglet.shapes.Star(x=70, y=70, outer_radius=60, inner_radius=35,
+                                       rotation=270, num_spikes=3,
+                                       color=(255, 255, 78), batch=batch)
+        self.vx = 1
+        self.vy = 1
+
+    def udpate(self):
+        self.body.x += self.vx
+        self.body.y += self.vy
+        pass
+
+    def delete(self):
+        self.body.delete()
+
+a = np.array([Car(),0,0,0])
+
+@window.event
+def on_draw():
+    window.clear()
+
+    if a[0].body.y > 150:
+        a[0].delete()
+        
+    a[0].udpate()
+    print (isinstance(a[0],Car))
+
+    batch.draw()
+
+
+pyglet.app.run()
